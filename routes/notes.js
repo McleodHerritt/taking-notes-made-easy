e4d315455748e9640550cee37f84c7fa3fac2c34
McleodHerritt <nmcleod2021@gmail.com>
2023-10-09 12:51:48 -0300

    Can now add new notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,36 +1,63 @@
 const notes = require("express").Router();
 const { v4: uuidv4 } = require("uuid");
-const { readAndAppend, readFromFile } = require("../helpers/fsUtils");
+const {
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require("../helpers/fsUtils");
 
-// GET Route for retrieving all the feedback
-notes.get("/", (req, res) =>
-  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)))
-);
+// GET Route for retrieving all the tips
+notes.get("/", (req, res) => {
+  readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
+});
 
-// POST Route for submitting feedback
+// GET Route for a specific note
+notes.get("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      const result = json.filter((note) => note.note_id === noteId);
+      return result.length > 0
+        ? res.json(result)
+        : res.json("No note with that ID");
+    });
+});
+
+// DELETE Route for a specific note
+notes.delete("/:note_id", (req, res) => {
+  const noteId = req.params.note_id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.note_id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile("./db/db.json", result);
+
+      // Respond to the DELETE request
+      res.json(`Item ${noteId} has been deleted 🗑️`);
+    });
+});
+
+// POST Route for a new note
 notes.post("/", (req, res) => {
-  // Destructuring assignment for the items in req.body
+  console.log(req.body);
+
   const { title, text } = req.body;
 
-  // If all the required properties are present
-  if (title && text) {
-    // Variable for the object we will save
+  if (req.body) {
     const newNote = {
       title,
       text,
       note_id: uuidv4(),
     };
 
-    readAndAppend(newFeedback, "./db/db.json");
-
-    const response = {
-      status: "success",
-      body: newNote,
-    };
-
-    res.json(response);
+    readAndAppend(newNote, "./db/db.json");
+    res.json(`Note added successfully`);
   } else {
-    res.json("Error in posting note");
+    res.error("Error in adding note");
   }
 });
 
